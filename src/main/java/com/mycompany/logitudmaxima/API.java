c88a8625b0e5644a4b2f06cecde52ba9cc7ffae4
DianaSanchezM <diana.sanchez-m@mail.escuelaing.edu.co>
2018-05-10 22:37:40 -0500

    dependencias en el pom

---
+++ src/main/java/com/mycompany/logitudmaxima/API.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.logitudmaxima;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author diana
+ */
+@RestController
+@RequestMapping(value = "/")
+public class API {
+    @RequestMapping(path = "/response/{numeros}", method = RequestMethod.GET)
+    public ResponseEntity<?> manejadorGetOrdersAPI(@PathVariable String numero) {
+        try {
+            //obtener datos que se enviarán a través del API
+            return new ResponseEntity<>(HttpServer.getValue(numero), HttpStatus.ACCEPTED);
+        } catch (Exception ex) {
+            //
+        }
+        return null;
+    }
+    
+    
+}
--- src/main/java/com/mycompany/logitudmaxima/HttpServer.java
+++ src/main/java/com/mycompany/logitudmaxima/HttpServer.java
@@ -5,83 +5,18 @@
  */
 package com.mycompany.logitudmaxima;
 
-import java.net.*;
-import java.io.*;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
+
 /**
  *
  * @author diani
  */
-public class HttpServer implements Runnable {
+public class HttpServer  {
 
-    private ServerSocket clientSocket = null;
+    
 
-    public HttpServer(ServerSocket clientSocket) {
-        this.clientSocket = clientSocket;
-    }
-
-
-    @Override
-    public void run(){
-        try {
-            while (true){
-                Socket socket = null;
-                socket = clientSocket.accept();
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                String inputLine, outputLine,datos;
-                while ((inputLine = in.readLine()) != null) {
-                   
-                    if (inputLine.contains("GET")) {
-                        String input = inputLine.split(" ")[1];
-                        if (input.equals("/") || input.equals("/index.html")){
-                            URL file = HttpServer.class.getResource("/index.html");
-                            datos = "";
-                            BufferedReader br = new BufferedReader(new InputStreamReader(file.openStream()));
-                            String texto;
-                            while ((texto = br.readLine()) != null){
-                                datos+=texto;
-                            }
-                            br.close();
-                            outputLine = "HTTP/1.1 200 OK\r\n"
-                                    + "Content-Type: "
-                                    + "text/html"
-                                    +"\r\n\r\n"
-                                    + datos;
-                            out.println(outputLine);
-                        }else if (input.contains("/response")){
-                            outputLine = "HTTP/1.1 200 OK\r\n"
-
-                                    + "Content-Type: " 
-
-                                    + "text/html"
-
-                                    + "\r\n\r\n"
-
-                                    +getValue(input.split("/")[2]); 
-
-                            out.println(outputLine);
-                        }
-                    }
-                    if (!in.ready()){
-                        break;
-                    }
-                    if (inputLine.equals("")){
-                        break;
-                    }
-                } 
-                out.close();
-                in.close();
-                socket.close();
-            }
-        }catch (IOException e){
-            Logger.getLogger(HttpServer.class.getName()).log(Level.SEVERE, null, e);
-        }
-    }
-
-    private String getValue(String valores) {
+    public static String getValue(String valores) {
         valores= valores.split("=")[1];
         String [] lista = valores.split("%2C");
         ArrayList<Integer> arreglo = new ArrayList<>();
